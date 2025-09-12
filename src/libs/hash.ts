e11c0af39d8a07a077a01ea67c9f800e63590877
Sal Scrudato <sal.scrudato@gmail.com>
2025-09-12 05:38:45 -0400

    Remove all existing files for clean start

--- src/libs/hash.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from 'crypto';
-export function sha256(obj: unknown) {
-  const json = typeof obj === 'string' ? obj : JSON.stringify(obj, Object.keys(obj as any).sort());
-  return createHash('sha256').update(json).digest('hex');
-}
